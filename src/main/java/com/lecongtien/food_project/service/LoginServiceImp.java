d6a638514a57f9f5cc97eacd6f27828d69fbefff
Cong-Tien <letien18102001@gmail.com>
2022-11-21 09:44:32 +0700

    feature/login

---
+++ src/main/java/com/lecongtien/food_project/service/LoginServiceImp.java
@@ -0,0 +1,19 @@
+package com.lecongtien.food_project.service;
+
+import com.lecongtien.food_project.entity.UserEntity;
+import com.lecongtien.food_project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoginServiceImp implements LoginService{
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public boolean checkLogin(String email, String password) {
+        List<UserEntity> users = userRepository.findByEmailAndPassword(email,password);
+        return users.size() > 0;
+    }
+}
